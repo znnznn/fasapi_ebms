49c8ccbbd3435299a97487ba525bf9e773b69a74
znnznn <znnznn@ukr.net>
2024-04-03 17:14:36 +0300

    fix websockets disconect

--- websockets_connection/routers.py
+++ websockets_connection/routers.py
@@ -13,7 +13,8 @@ router = APIRouter()
 async def websocket_endpoint(websocket: WebSocket, user: User = Depends(get_auth_user_by_websocket)):
     await connection_manager.connect(websocket, "orders")
     try:
-        await websocket.receive_text()
+        while True:
+            data = await websocket.receive_text()
     except (WebSocketException, WebSocketDisconnect):
         await connection_manager.disconnect(websocket, "orders")
         return
