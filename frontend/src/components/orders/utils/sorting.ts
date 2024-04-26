84a6dc70cba6764f7432bfe1b8ead70623bfd870
nazar <nick20name17@gmail.com>
2024-04-26 22:24:15 +0300

    fix origin_items ordering

--- frontend/src/components/orders/utils/sorting.ts
+++ frontend/src/components/orders/utils/sorting.ts
@@ -3,40 +3,94 @@ import type { Row } from '@tanstack/react-table'
 import type { OriginItems } from '@/store/api/ebms/ebms.types'
 
 export const statusFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
-    const statusA = rowA.original.item?.stage?.name as string
-    const statusB = rowB.original.item?.stage?.name as string
+    const statusA = rowA.original.item?.stage?.name?.toLowerCase() ?? ''
+    const statusB = rowB.original.item?.stage?.name?.toLowerCase() ?? ''
+    const order = {
+        unscheduled: 0,
+        done: 2
+    } as Record<string, number>
 
-    const order = ['unscheduled', 'assigned', 'unscheduled', 'wrapped', 'bent', 'done']
+    const orderA = order[statusA] !== undefined ? order[statusA] : 1
+    const orderB = order[statusB] !== undefined ? order[statusB] : 1
 
-    return order.indexOf(statusA?.toLowerCase()) - order.indexOf(statusB?.toLowerCase())
+    if (orderA === orderB) {
+        return statusA?.localeCompare(statusB)
+    }
+
+    return orderA - orderB
 }
 
 export const dateFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
-    const dateA = rowA.original.item?.production_date as string
-    const dateB = rowB.original.item?.production_date as string
+    const dateA = rowA.original?.item?.production_date ?? '0'
+    const dateB = rowB.original?.item?.production_date ?? '0'
 
-    return new Date(dateA).getTime() - new Date(dateB).getTime()
+    return new Date(dateA)?.setHours(0, 0, 0, 0) - new Date(dateB)?.setHours(0, 0, 0, 0)
 }
 
 export const widthLengthFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
-    const widthA = +rowA.original.width
-    const widthB = +rowB.original.width
-    const lengthA = +rowA.original.length
-    const lengthB = +rowB.original.length
+    const widthA = +rowA.original?.width || 0
+    const widthB = +rowB.original?.width || 0
+    const lengthA = +rowA.original?.length || 0
+    const lengthB = +rowB.original?.length || 0
 
     return widthA + lengthA - widthB + lengthB
 }
 
 export const notesFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
-    const notesA = rowA.original.item?.comments
-    const notesB = rowB.original.item?.comments
+    const notesA = rowA.original?.item?.comments
+    const notesB = rowB.original?.item?.comments
 
-    return notesA.length - notesB.length
+    return notesA?.length || 0 - notesB?.length || 0
 }
 
 export const flowFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
-    const flowA = rowA.original.item?.flow?.name
-    const flowB = rowB.original.item?.flow?.name
+    const flowA = rowA.original?.item?.flow?.name
+    const flowB = rowB.original?.item?.flow?.name
 
-    return flowA.localeCompare(flowB)
+    if (flowA === undefined && flowB === undefined) {
+        return 0
+    } else if (flowA === undefined) {
+        return -1
+    } else if (flowB === undefined) {
+        return 1
+    } else {
+        return flowA.localeCompare(flowB)
+    }
+}
+
+export const priorityFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
+    const priorityA = rowA.original?.item?.priority || 0
+    const priorityB = rowB.original?.item?.priority || 0
+
+    return priorityB - priorityA
+}
+
+export const locationFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
+    const locationA = rowA.original?.item?.location || 0
+    const locationB = rowB.original?.item?.location || 0
+
+    return locationB - locationA
+}
+
+export const packagesFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
+    const packagesA = rowA.original?.item?.packages || 0
+    const packagesB = rowB.original?.item?.packages || 0
+
+    return packagesB - packagesA
+}
+
+export const timeFn = (rowA: Row<OriginItems>, rowB: Row<OriginItems>) => {
+    const [hour, minute, second] = rowA.original?.item?.time?.split(':')?.map(Number) ?? [
+        0, 0, 0
+    ]
+
+    const timeA = new Date()?.setHours(hour, minute, second)
+
+    const [hourB, minuteB, secondB] = rowB.original?.item?.time
+        ?.split(':')
+        ?.map(Number) ?? [0, 0, 0]
+
+    const timeB = new Date()?.setHours(hourB, minuteB, secondB)
+
+    return timeA - timeB
 }
